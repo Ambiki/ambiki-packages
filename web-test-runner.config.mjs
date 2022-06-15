2cf5db1efe51837f6f7811fa6f4f52333dffdd65
abeidahmed <abeidahmed92@gmail.com>
2022-06-15 09:42:58 +0400

    feat: add combobox package

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,17 +1,18 @@
 import fs from 'fs';
 import { esbuildPlugin } from '@web/dev-server-esbuild';
 
-const customElements = fs
-  .readdirSync('custom-elements')
-  .filter((dir) => fs.statSync(`custom-elements/${dir}`).isDirectory());
+const filteredDir = (workspace) =>
+  fs.readdirSync(workspace).filter((dir) => fs.statSync(`${workspace}/${dir}`).isDirectory());
+
+const workspaces = [...filteredDir('custom-elements'), ...filteredDir('packages')];
 
 export default {
   concurrency: 10,
   nodeResolve: true,
   watch: process.argv.includes('--watch'),
-  groups: customElements.map((pkg) => ({
+  groups: workspaces.map((pkg) => ({
     name: pkg,
-    files: `custom-elements/${pkg}/spec/**/*.spec.ts`,
+    files: `**/${pkg}/spec/**/*.spec.ts`,
   })),
   plugins: [esbuildPlugin({ ts: true })],
 };
