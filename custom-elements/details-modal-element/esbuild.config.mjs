992b6e5934ca1b08ed9a6514fe3d282878c13f4e
abeidahmed <abeidahmed92@gmail.com>
2022-06-19 11:33:49 +0400

    app default object

--- custom-elements/details-modal-element/esbuild.config.mjs
+++ custom-elements/details-modal-element/esbuild.config.mjs
@@ -9,7 +9,7 @@ esbuild
     watch: process.argv.includes('--watch'),
     bundle: true,
     minify: true,
-    external: Object.keys(JSON.parse(fs.readFileSync(path.resolve('./package.json'), 'utf-8')).dependencies),
+    external: Object.keys(JSON.parse(fs.readFileSync(path.resolve('./package.json'), 'utf-8')).dependencies || {}),
     format: 'esm',
   })
   .catch(() => process.exit(1));
