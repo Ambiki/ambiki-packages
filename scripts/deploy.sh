3222dbfd3cdfe71d798681c73e38496df37eeee1
abeidahmed <abeidahmed92@gmail.com>
2023-03-02 19:15:35 +0400

    Add deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+yarn docs:build-safe
+
+# navigate into the build output directory
+cd docs/.vuepress/dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:Ambiki/ambiki-packages.git main:gh-pages
+
+cd -
